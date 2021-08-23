93e19f48fc1ef471da0347c1f2ba3005331e6a60
heyangzhou <he.yangzhou@redstarclouds.com>
2021-08-23 09:15:23 +0800

    feat: threejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {defineComponent} from 'vue';
-import Cube from './packages/cubeDemo/Cube';
+import CubeNative from './packages/cubeDemo/Cube';
+import Cube from './packages/three/cube/Cube';
 import Rect from '@/packages/rect/Rect';
 
 export default defineComponent({
@@ -7,8 +8,12 @@ export default defineComponent({
   setup() {
     return ()=> (
       <div>
+        <p>webGl立方体:</p>
+        <CubeNative ></CubeNative>
+        <p>threejs立方体:</p>
         <Cube />
         <Rect></Rect>
+
       </div>
     );
   },
