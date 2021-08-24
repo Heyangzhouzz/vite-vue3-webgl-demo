400467cc8bdc642660caab16910084bcd95fb370
heyangzhou <he.yangzhou@redstarclouds.com>
2021-08-24 09:29:58 +0800

    feat: ball

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {defineComponent} from 'vue';
 import CubeNative from './packages/cubeDemo/Cube';
 import Cube from './packages/three/cube/Cube';
+import Ball from './packages/three/ball/Ball';
 import Rect from '@/packages/rect/Rect';
 
 export default defineComponent({
@@ -12,6 +13,7 @@ export default defineComponent({
         <CubeNative ></CubeNative>
         <p>threejs立方体:</p>
         <Cube />
+        <Ball />
         <Rect></Rect>
 
       </div>
---
+++ src/packages/three/ball/Ball.tsx
@@ -0,0 +1,57 @@
+import {defineComponent, onMounted, ref} from 'vue';
+import {
+  Scene, BoxGeometry, MeshLambertMaterial,
+  Mesh, PointLight, OrthographicCamera, WebGLRenderer, SphereGeometry, MeshPhongMaterial,
+} from 'three';
+
+export default defineComponent({
+  name: 'Ball',
+  setup() {
+    const glCanvas = ref<HTMLCanvasElement|null>(null);
+    const scene = new Scene();
+
+    {
+      const box = new BoxGeometry(100, 100, 100);
+      const material = new MeshLambertMaterial({color: 0x00eeff});
+      const mesh = new Mesh(box, material);
+      scene.add(mesh);
+    }
+
+    {
+      const box = new SphereGeometry(60, 40, 40);
+      const material = new MeshPhongMaterial({color: 0x0000ff, specular: 0x4488ee, shininess: 12});
+      const mesh = new Mesh(box, material);
+      mesh.translateY(100);
+      scene.add(mesh);
+    }
+
+    const point = new PointLight(0xffeeff);
+    point.position.set(400, 200, 300);
+    scene.add(point);
+
+    onMounted(() => {
+      if (!glCanvas.value) {
+        return;
+      }
+      const width = glCanvas.value.clientWidth;
+      const height = glCanvas.value.clientHeight;
+      const k = width/height;
+      const s = 200;
+
+      const camera = new OrthographicCamera(-s * k, s * k, s, -s, 1, 2000);
+      camera.position.set(200, 200, 200);
+      camera.lookAt(scene.position);
+
+      const render = new WebGLRenderer({canvas: glCanvas.value, alpha: true});
+      console.log(render);
+      render.setClearColor(0xb9d3ff, 1);
+
+      render.render(scene, camera);
+    });
+    return ()=> (
+      <div>
+        <canvas ref={glCanvas} id="glcanvas" width="640" height="640"></canvas>
+      </div>
+    );
+  },
+});
